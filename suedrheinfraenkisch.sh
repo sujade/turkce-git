961322d1d87a6a9ed86d2ff07a3922a937f4a132
Simon Hörrle <simon.hoerrle@gmail.com>
2023-08-24 22:44:26 +0200

    Add Südrheinfränkisch

---
+++ suedrheinfraenkisch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+git config --global alias.mach-uff init
+git config --global alias.mach-noch clone
+git config --global alias.zieg pull
+git config --global alias.do-daezu add
+git config --global alias.drigg push
+git config --global alias.schlombar 'push --force'
+git config --global alias.aschd branch
+git config --global alias.vaaeschdlae branch
+git config --global alias.reich-ei commit
+git config --global alias.sordier-nei rebase
+git config --global alias.unnascheid diff
+git config --global alias.do-zommae merge
+git config --global alias.bungar stash
+git config --global alias.edikedier tag
+git config --global alias.hol-raus checkout
+git config --global alias.brodokoll log
+git config --global alias.zuschdond status
+git config --global alias.schwaerz-o blame
+
+echo "alias git='simbl'" >> ~/.bashrc

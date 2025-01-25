2032b88fc7c8759445539ca4787f6ab65246e9cd
sujade <dnmzsu@gmail.com>
2025-01-25 13:44:44 +0100

    remove redundant files

---
+++ turkce.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+git config --global alias.ac init
+git config --global alias.nusha clone
+git config --global alias.cek pull
+git config --global alias.ekle add
+git config --global alias.it push
+git config --global alias.zorla-it 'push --force'
+git config --global alias.dal branch
+git config --global alias.dallandir branch
+git config --global alias.isle commit
+git config --global alias.temel-at rebase
+git config --global alias.ayirt-et diff
+git config --global alias.birlestir merge
+git config --global alias.zulala stash
+git config --global alias.etiketle tag
+git config --global alias.teslim-al checkout
+git config --global alias.kutuk log
+git config --global alias.durum status
+git config --global alias.sucla blame
+git config --global alias.uzaktan remote
